ac73620e310b853e3fc0f03f865d912e8eb882b7
Nages Desaraju <desaraju02@gmail.com>
2017-07-11 16:43:51 +0530

    Adding JUnits

---
+++ src/com/nag/test/JenkinsAPIClientTest.java
@@ -0,0 +1,16 @@
+package com.nag.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class JenkinsAPIClientTest {
+
+	@Test
+	public void testListJobs() {
+		//fail("Not yet implemented");
+		
+		assertEquals("JenkinsAPI", "JenkinsAPI");
+	}
+
+}
